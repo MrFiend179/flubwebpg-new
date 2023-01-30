065e6c2dc15e9929326fdca61c4c36e330be635a
MrFiend179 <yibolaxinw@gmail.com>
2023-01-30 20:40:14 +0500

    addldrsc

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+window.addEventListener("load", function(){
+  // show the spinner
+  document.querySelector(".spinner").style.display = "flex";
+  // hide the spinner after the video has loaded
+  document.querySelector("video").addEventListener("loadeddata", function(){
+    document.querySelector(".spinner").style.display = "none";
+  });
+});
+
+
 
 mcnav.addEventListener('mouseover', function handleMouseOver(){
     console.log('ok')
